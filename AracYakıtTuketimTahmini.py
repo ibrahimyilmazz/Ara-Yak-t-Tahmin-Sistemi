74ba4b8351d73583850566c5612a69e05df0b63d
ibrahim <48454178+ibrahimyilmazz@users.noreply.github.com>
2022-04-09 01:16:49 +0300

    Update AracYakıtTuketimTahmini.py

--- AracYakıtTuketimTahmini.py
+++ AracYakıtTuketimTahmini.py
@@ -2,27 +2,19 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 from scipy import stats
-from scipy.stats import norm, skew   # Istatistik işlemleri için; verinin feature engineering işlemleri için
-
+from scipy.stats import norm, skew   
 from sklearn.preprocessing import RobustScaler, StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
-from sklearn.model_selection import train_test_split, GridSearchCV   # Veriyi train ve test olarak bölebilmek için
+from sklearn.model_selection import train_test_split, GridSearchCV   
 from sklearn.metrics import mean_squared_error
-from sklearn.base import clone # tüm modelleri birleştirirken kullandık
-
-
-# XGBoost
+from sklearn.base import clone 
 import xgboost as xgb
-
-# warning
 import warnings
 warnings.filterwarnings('ignore')
 
 
-column_name = ["MPG", "Cylinders", "Displancement", "Horsepower", "Weight", "Acceleration", "Model Year", "Origin"] # araç isimleri ile ilgilenmiyoruz
+column_name = ["MPG", "Cylinders", "Displancement", "Horsepower", "Weight", "Acceleration", "Model Year", "Origin"] 
 data = pd.read_csv("auto-mpg.data", names = column_name, na_values= "?", comment="\t", sep = " ", skipinitialspace = True)
 
 print(data.head())
@@ -31,24 +23,20 @@ data.info()
 
 describe = data.describe() 
 
-
-#missing value - eksik veriler
+#missing value 
 print(data.isna().sum())  
 
-#horsepower içindeki eksik verileri ortalama değerler ile doldurma
+#horsepower içindeki eksik verileri ortalama degerler ile doldurma
 data["Horsepower"] = data["Horsepower"].fillna(data["Horsepower"].mean())
 
-#missing value - eksik veriler - tekrar kontrol için
-print(data.isna().sum()) 
 
-# Dağılıma bakmak için; NaN değer olunca göstermiyor.
 sns.distplot(data.Horsepower) # Kuyruk sağa doğru
 
 
+# %% 
+# EDA
 
-#verinin incelenmesi - EDA
-
-#nümerik veriler için corelasyon matrisinin oluşturulması
+# Nümerik veriler için corelasyon matrisinin oluşturulması
 corr_matrix = data.corr()
 sns.clustermap(corr_matrix, annot = True, fmt = ".2f")
 plt.title("Correlation btw features")
@@ -63,19 +51,12 @@ plt.title("Correlation btw features")
 plt.show()
 
 """
-multicollinearlity (eş düzlem var)
-yani benzer özellikte değerler, bunlar da modelimizi yaniltacaktir
+multicollinearlity (es düzlem var)
+yani benzer özellikte degerler, bunlar da modelimizi yaniltacaktir
 """
-
 
 sns.pairplot(data, diag_kind = "kde", markers = "+")
 plt.show()
-
-"""
-cylinders ve origin değerleri kategorikal olabilir. 
-İlerleyen aşamalarda kategorik değişkenlere çevireceğiz (Özellik Mühendisliği)
-"""
-
 
 
 plt.figure()
@@ -87,24 +68,19 @@ sns.countplot(data["Origin"])
 print(data["Origin"].value_counts())
 
 
-
 #box
 for c in data.columns:
     plt.figure()
     sns.boxplot(x = c, data = data, orient = "v") 
 
+    
 """
 IQR = Q3 - Q1   |||    Q3 + 1.5 x IQR = Üst Sınır     
                 |||    Q1 - 1.5 x IQR = Alt Sınır
-
-Acceleration ve Horspower verilerinde aykırı değerlerin (outliers) fazla olduğunu görüyoruz. Onları temizleyeceğiz
-
 """
 
-
-
 #Outlier
-thr = 2   # Genelde alt sınır olarak 1.5 değeri alınır; veri az old. icin 2 aldim
+thr = 2   # veri az old. icin 2 aldindi
 
 horspower_desc = describe["Horsepower"]
 q3_hp = horspower_desc[6]
@@ -116,7 +92,7 @@ filter_hp_bottom = bottom_limit_hp < data["Horsepower"]
 filter_hp_top = data["Horsepower"] < top_limit_hp
 filter_hp = filter_hp_bottom & filter_hp_top
 
-data = data[filter_hp]      # 1 aykırı değer silindi
+data = data[filter_hp]      # 1 aykırı deger silindi
 
 
 acceleration_desc = describe["Acceleration"]
@@ -129,18 +105,9 @@ filter_acc_bottom = bottom_limit_acc < data["Acceleration"]
 filter_acc_top = data["Acceleration"] < top_limit_acc
 filter_acc = filter_acc_bottom & filter_acc_top
 
-data = data[filter_acc]     # 2 aykırı değer silindi
+data = data[filter_acc]     # 2 aykırı deger silindi
 
-
-
-"""
-Verinin 1. momenti ortalamayı
-Verinin 2. momenti standart sapmayı 
-Verinin 3. momenti çarpıklığı (skewed) verir
-
-çarpıklık >  1 ise pozitif çarpıklık (kuyruk saga dogru)
-çarpıklık < -1 ise negatif çarpıklık (kuyruk sola dogru)
-"""
+# %%
 ## Feature Engineering
 # Skewness
 
@@ -150,48 +117,41 @@ sns.distplot(data.MPG, fit = norm)
 (mu, sigma) = norm.fit(data["MPG"])
 print("mu: {}, sigma = {}".format(mu, sigma))
 # mu: 23.472405063291134, sigma = 7.756119546409932
-# mu = ortalama           sigma = standart sapma
 
 #qq plot
 plt.figure()
 stats.probplot(data["MPG"], plot = plt)
-plt.show()          # veri kırmızı çizgi üzerinde olsaydı normal dağılımda olmuş olacaktı
+plt.show()          
 
 
-data["MPG"] = np.log1p(data["MPG"])     # log1p kullandık
+data["MPG"] = np.log1p(data["MPG"])     
 plt.figure()
-sns.distplot(data.MPG, fit = norm)      # Çarpıklık azaldı
+sns.distplot(data.MPG, fit = norm)      
 
 (mu, sigma) = norm.fit(data["MPG"])
-print("mu: {}, sigma = {}".format(mu, sigma))   # yeni mu ve sigma değerleri daha iyi
+print("mu: {}, sigma = {}".format(mu, sigma))   
 # mu: 3.146474056830183, sigma = 0.3227569103044823
 
 plt.figure()
 stats.probplot(data["MPG"], plot = plt)
-plt.show()          # yeni normallik değeri daha iyi
+plt.show()          
 
 
 # feature - independent variable
-skewed_feats = data.apply(lambda x: skew(x.dropna())).sort_values(ascending = False) # ascending = artan; azalan bir sıralama yaptık
+skewed_feats = data.apply(lambda x: skew(x.dropna())).sort_values(ascending = False) 
 skewness = pd.DataFrame(skewed_feats, columns = ["skewed"]) 
-# Horspower da pozitif sekness var çünkü değeri 1 den büyük | 1.06 değeri 1'e çok yakın olduğu için düzeltmedik
-
 
 
 # one hot encoding
-"""
-one hot encoding uygulayabilmek için önce kategorik hale getirmek gerekiyor
-"""
 data["Cylinders"] = data["Cylinders"].astype(str) 
 data["Origin"] = data["Origin"].astype(str)
 
 data = pd.get_dummies(data)
 
 
-
 # Split 
-x = data.drop(["MPG"], axis = 1)    # independent variable
-y = data.MPG                        # dependent variable
+x = data.drop(["MPG"], axis = 1)    
+y = data.MPG                        
 
 test_size = 0.9 #veriyi zorlamak için az veri ile eğitim
 X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size = test_size, random_state = 42)
@@ -213,7 +173,6 @@ print("LR Coef :", lr.coef_)
 y_predicted_dummy = lr.predict(X_test)
 mse = mean_squared_error(Y_test, y_predicted_dummy)
 print("linear Regression MSE :", mse) # linear Regression MSE : 0.020632204780133005
-
 
 
 
@@ -296,7 +255,6 @@ print("ElasticNet MSE: ", mse)
 
 
 # %%
-
 # Standardization
 scaler = RobustScaler()
 X_train = scaler.fit_transform(X_train)
@@ -475,25 +433,3 @@ RobustScaler
     XGBRegressor MSE:  0.017444718427058307
     Averaged Models MSE:  0.016551165591244896
 """
-
-
-
-
-
-# %%
-
-# Tahmin Deneme
-test = [71, 65, 1773, 19, 71, 0, 1, 0, 0, 0, 0, 0, 1]    #3.46 --> Gerçek Degerler
-test = [121, 110, 2260, 14, 73, 0, 1, 0, 0, 0, 0, 1, 0]  #3.21
-test = [340, 160, 3609, 8, 70, 0, 0, 0, 0, 1, 1, 0, 0]   #2.70
-
-sutunlar = ['Displancement', 'Horsepower', 'Weight', 'Acceleration',
-       'Model Year', 'Cylinders_3', 'Cylinders_4', 'Cylinders_5',
-       'Cylinders_6', 'Cylinders_8', 'Origin_1', 'Origin_2', 'Origin_3']
-
-test2 = pd.Series(data=test, index=sutunlar)
-y_predicted_dummy = clf.predict(test2.to_numpy().reshape(1, -1))
-y_predicted_dummy
-
-
-
